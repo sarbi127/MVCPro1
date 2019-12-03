23c732a9984780e1674bc988c604518b74bdaff6
Sara Binazir <sarabinazir@gmail.com>
2019-12-03 16:22:59 +0100

    Add project files.

---
+++ MVCPro/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCPro.Models;
+
+namespace MVCPro.Controllers
+{
+    public class DemoController : Controller
+    {
+        //public string Index(int id, string name)
+        //{
+        //   // return View();
+        //    return HtmlEncoder.Default.Encode($"Demo index id:{id} name{name}");
+        //}
+
+        public IActionResult Index(int salary, string name)
+        {
+           var model = new Person()
+            {
+                Name = name,
+                Salary = salary
+            };
+
+            return View(model);
+
+        }
+    }
+}
---
+++ MVCPro/Data/MVCProContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCPro.Models
+{
+    public class MVCProContext : DbContext
+    {
+        public MVCProContext (DbContextOptions<MVCProContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCPro.Models.Person> Person { get; set; }
+    }
+}
